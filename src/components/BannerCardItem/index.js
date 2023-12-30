c6d721934bfce434566d9c761fc679e996789af6
pallavi4875 <pallaviyadav4875@gmail.com>
2023-12-30 09:41:56 +0000

    commit changes

--- src/components/BannerCardItem/index.js
+++ src/components/BannerCardItem/index.js
@@ -1,13 +1,14 @@
 // Write your code here.
 const BannerProfile = props => {
   const {BannerDetails} = props
-  const {id, headerText, description, className} = BannerDetails
+  const {id, headerText, description, className, button} = BannerDetails
   return (
-    <li className="BannerCardItem">
+    <li className={className}>
       {id}
       <div className={className}>
         <h1>{headerText}</h1>
         <p>{description}</p>
+        <button>show more</button>
       </div>
     </li>
   )
